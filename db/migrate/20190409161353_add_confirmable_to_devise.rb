56eccc195ad8038b0a89022d2a871c1122b2f678
stephanesdr <stadlerstephane@gmail.com>
2019-04-10 17:31:45 +0200

    clearing syntax migration to add confirmable to devise admin

--- db/migrate/20190409161353_add_confirmable_to_devise.rb
+++ db/migrate/20190409161353_add_confirmable_to_devise.rb
@@ -8,11 +8,5 @@ class AddConfirmableToDevise < ActiveRecord::Migration[5.2]
       t.datetime :confirmation_sent_at
       t.index :confirmation_token, unique: true
     end
-    # Admin.update_all confirmed_at: Time.zone.now
-  end
-
-  def down
-    remove_columns :admins, :confirmation_token, :confirmed_at, :confirmation_sent_at
-    # remove_columns :admins, :unconfirmed_email # Only if using reconfirmable
   end
 end
