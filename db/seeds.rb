a5c39153748a93f3828628005f23b585197bef8a
stephanesdr <stadlerstephane@gmail.com>
2019-04-12 14:54:05 +0200

    unquote user and amdin (only for dev)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,25 @@
   p "ITEM #{i} : créé"
 end
 
-1.upto(10) do |i|
-  User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password(8)
-  )
-  p "ITEM #{i} : créé"
-end
+# 1.upto(10) do |i|
+#   User.create!(
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password(8)
+#   )
+#   p "ITEM #{i} : créé"
+# end
 
-el = 1
-while el <= 10
-  Profile.create!(
-    user_id: el
-  )
-  p "profile #{el}"
-  el += 1
-end
+# el = 1
+# while el <= 10
+#   Profile.create!(
+#     user_id: el
+#   )
+#   p "profile #{el}"
+#   el += 1
+# end
 
-Admin.create(
-  email: 'sdrmvc@yopmail.com',
-  password: 'password',
-  password_confirmation: 'password',
-)
+# Admin.create(
+#   email: 'sdrmvc@yopmail.com',
+#   password: 'password',
+#   password_confirmation: 'password',
+# )
