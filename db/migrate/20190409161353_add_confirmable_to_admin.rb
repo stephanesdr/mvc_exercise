13f2855abca8f0a8e7e6f3d2effa5b565b3535ec
stephanesdr <stadlerstephane@gmail.com>
2019-04-12 14:38:42 +0200

    fix name migration and his  content

--- db/migrate/20190409161353_add_confirmable_to_admin.rb
+++ db/migrate/20190409161353_add_confirmable_to_admin.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-class AddConfirmableToDevise < ActiveRecord::Migration[5.2]
+class AddConfirmableToAdmin < ActiveRecord::Migration[5.2]
+  # Note: You can't use change, as User.update_all will fail in the down migration
   def up
     change_table :admins, bulk: true do |t|
       t.string :confirmation_token
@@ -8,5 +9,9 @@ class AddConfirmableToDevise < ActiveRecord::Migration[5.2]
       t.datetime :confirmation_sent_at
       t.index :confirmation_token, unique: true
     end
+  end
+
+  def down
+    remove_columns :admins, :confirmation_token, :confirmed_at, :confirmation_sent_at
   end
 end
